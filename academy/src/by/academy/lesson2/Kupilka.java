3c2e253cc3c082cb9feca814f8d9fe82b5f153a2
Pasha <Pasha@Pasha-PC>
2021-01-20 14:23:38 +0300

    Homework (Tasks #1,2)

---
+++ academy/src/by/academy/lesson2/Kupilka.java
@@ -0,0 +1,42 @@
+package by.academy.lesson2;
+
+import java.util.Scanner;
+
+public class Kupilka {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        double sum;
+        System.out.println("Введите сумму покупки без скидки: ");
+        sum = num.nextDouble();
+
+        if (sum < 100) {
+            double sumPokupki = sum - ((sum / 100) * 5);
+            System.out.println("Сумма покупки со скидкой = " + sumPokupki);
+        } else if (sum >= 100 && sum < 200) {
+            double sumPokupki = sum - ((sum / 100) * 7);
+            System.out.println("Сумма покупки со скидкой = " + sumPokupki);
+        } else if (sum >= 200 && sum < 300) {
+            Scanner num1 = new Scanner(System.in);
+            short age;
+            System.out.println("Введите Ваше полное количество лет: ");
+            age = num.nextShort();
+
+            if (age > 18) {
+                double sumPokupki = sum - ((sum / 100) * 16);
+                System.out.println("Сумма покупки со скидкой = " + sumPokupki);
+            } else {
+                double sumPokupki = sum - ((sum / 100) * 8);
+                System.out.println("Сумма покупки со скидкой = " + sumPokupki);
+            }
+
+        }
+        if (sum >= 300 && sum < 400) {
+            double sumPokupki = sum - ((sum / 100) * 15);
+            System.out.println("Сумма покупки со скидкой = " + sumPokupki);
+        } else if (sum >= 400) {
+            double sumPokupki = sum - ((sum / 100) * 20);
+            System.out.println("Сумма покупки со скидкой = " + sumPokupki);
+        }
+
+    }
+}
---
+++ academy/src/by/academy/lesson2/Switch.java
@@ -0,0 +1,49 @@
+package by.academy.lesson2;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in).useLocale(Locale.ENGLISH);
+        System.out.println("¬ведите тип данных int, double, float, char или String: ");
+        String a = num.nextLine();
+
+        switch (a) {
+            case "int":
+                System.out.println("¬ведите число соответствующего типа: ");
+                int b = num.nextInt();
+                int ost;
+                ost = b % 2;
+                System.out.println(ost);
+                break;
+            case "double":
+                System.out.println("¬ведите число соответствующего типа: ");
+                double с = num.nextDouble();
+                double proc;
+                proc = с / 100 * 70;
+                System.out.println(proc);
+                break;
+            case "float":
+                System.out.println("¬ведите число соответствующего типа: ");
+                float d = num.nextFloat();
+                float kv = (float) Math.pow(d, 2);
+                System.out.println(kv);
+                break;
+            case "char":
+                System.out.println("¬ведите один произвольный символ: ");
+                String e = num.nextLine();
+                char cod = e.charAt(0);
+                int cod1 = (int) cod;
+                System.out.println(cod1);
+                break;
+            case "String":
+                System.out.println("¬ведите произвольный набор данных: ");
+                String f = num.nextLine();
+                System.out.println("Hello " + f);
+                break;
+            default:
+                System.out.println("Unsupported type");
+        }
+    }
+}
